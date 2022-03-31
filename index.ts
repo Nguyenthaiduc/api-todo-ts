84be21eda5aea21b1a368abce3ec8f205045389c
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-03-31 08:45:32 +0700

    add models , routes

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
-import express,{Request,Response} from 'express';
-
+import express,{Request,Response,Application} from 'express';
+import logger from 'morgan';
 const app = express();
 const port : number = 5000
+
+//view engine
+export = function(app : Application){
+    app.use(logger("Something a Wrong View Engine"));
+    app.use(express.json())
+    app.use(express.urlencoded({ extended : true }));
+}
 
 app.get('/', (req:Request, res:Response) => {
     res.send("Hello World")
---
+++ models/todo.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema
+
+//create schema todo namespace dùng để gom nhóm các table có chung một đặc điểm
+const TodoSchema = new Schema({
+    todo:{
+        type: 'string',
+        required:[true,'todo text field is required']
+    }
+})
+
+//create model todo
+const Todos = mongoose.model('todo',TodoSchema)
+export default Todos
