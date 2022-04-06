f5b1f4d8d77230066f677da93ef28d57c293c658
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-04-06 18:25:23 +0700

    update model

--- models/todo.ts
+++ models/todo.ts
@@ -3,10 +3,16 @@ const Schema = mongoose.Schema
 
 //create schema todo namespace dùng để gom nhóm các table có chung một đặc điểm
 const TodoSchema = new Schema({
+    
     todo:{
         type: 'string',
         required:[true,'todo text field is required']
+    },
+    isDone:{
+        type : Boolean,
+        default: false
     }
+    
 })
 
 //create model todo
